ad695f6da7895fc2e66c3caa4aba30b8a00cdeaf
Dhire079 <51140332+Dhire079@users.noreply.github.com>
2020-12-15 22:09:36 -0500

    Add files via upload

---
+++ force-app/main/default/lwc/fscAccountTable/fscAccountTable.js
@@ -0,0 +1,79 @@
+import { LightningElement,track,wire } from 'lwc';
+import getFSCAccounts from '@salesforce/apex/FscAccountTableController.getFSCAccounts';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent'
+
+const columns = [
+    { label: 'Account Name', fieldName: 'nameurl__c', sortable: 'true', editable: true,type:'url',
+    typeAttributes: {
+        label: { fieldName: 'Name' }, target: '_blank', tooltip: { fieldName: 'Name' } 
+    }},
+    { label: 'Owner', fieldName: 'OwnerId', sortable: 'true', editable: true},
+    { label: 'Phone', fieldName: 'Phone', editable: true},
+    { label: 'Website', fieldName: 'Website' , editable: true},
+    { label: 'Annual Revenue', fieldName: 'AnnualRevenue', editable: true }
+];
+    
+    export default class FscAccountTable extends LightningElement {
+
+    columns = columns;
+    sortBy;
+    sortDirection;
+    accountsList;
+    backupResponseFromWire;
+
+    @wire(getFSCAccounts, {})
+    wiredAccounts(resp) {
+
+        this.backupResponseFromWire = resp;
+        const { data, error } = resp;
+        if (data) 
+        {
+            console.log('Entered');
+            console.log('Data '+data);
+            this.accountsList = data;
+            
+
+        }
+        else if(error)
+        {
+        this.showNotification("error","Error getting Account data", error );
+        }
+    }
+
+    doSorting(event) {
+        this.sortBy = event.detail.fieldName;
+        this.sortDirection = event.detail.sortDirection;
+        this.sortData(this.sortBy, this.sortDirection);
+        }
+
+
+        sortData(fieldname, direction) {
+        let parseData = JSON.parse(JSON.stringify(this.accountsList));
+        // Return the value stored in the field
+        let keyValue = (a) => {
+        return a[fieldname];
+        };
+        // cheking reverse direction
+        let isReverse = direction === 'asc' ? 1: -1;
+        // sorting data
+        parseData.sort((x, y) => {
+        x = keyValue(x) ? keyValue(x) : ''; // handling null values
+        y = keyValue(y) ? keyValue(y) : '';
+        // sorting values based on direction
+        return isReverse * ((x > y) - (y > x));
+        });
+        this.accountsList = parseData;
+        }
+
+
+     // Show Toast Messages
+  showNotification(variant, title, message) {
+    const evt = new ShowToastEvent({
+      title: title,
+      message: message,
+      variant: variant
+    });
+    this.dispatchEvent(evt);
+  }
+}
+
